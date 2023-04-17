b15a8e5bec926a40021a6a3079954c65ad87f424
Aziza Zhalgaspayeva <duisembay12@gmail.com>
2023-04-17 18:15:43 +0600

    add models

---
+++ main/models/task.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+from .tag import Tag
+from .user import User
+
+
+class Task(models.Model):
+    class Status(models.TextChoices):
+        NEW_TASK = "new_task"
+        IN_DEVELOPMENT = "in_development"
+        ARCHIVED = "archived"
+        IN_QA = "in_qa"
+        IN_CODE_REVIEW = "in_code_review"
+        READY_FOR_RELEASE = "ready_for_release"
+        RELEASED = "released"
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_edited = models.DateTimeField(auto_now=True)
+    deadline = models.DateField(blank=True)
+    status = models.CharField(
+        max_length=255, default=Status.NEW_TASK, choices=Status.choices
+    )
+    priority = models.IntegerField()
+    tags = models.ManyToManyField(Tag)
+    reporter = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL, related_name="reporter"
+    )
+    assignee = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL, related_name="assignee"
+    )
