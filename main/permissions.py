76cc2eb589cefe508d1e4fd16d845a7e6209ef76
Aziza Zhalgaspayeva <duisembay12@gmail.com>
2023-05-10 16:37:29 +0600

    change permissions.py

--- main/permissions.py
+++ main/permissions.py
@@ -1,14 +1,7 @@
 from rest_framework.permissions import BasePermission
 
 class DeleteStaffOnly(BasePermission):
-
-    edit_methods = ("DELETE",)
-
     def has_permission(self, request, view):
-        if request.method not in self.edit_methods:
-            return True
-        
-        if request.user.is_staff:
-            return True
-        
-        return False
+        if request.method == "DELETE" and not request.user.is_staff:
+            return False      
+        return True
