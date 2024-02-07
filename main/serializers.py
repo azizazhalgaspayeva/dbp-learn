b5164206d7f47025b1e0169c897324b3703733b9
Aziza Zhalgaspayeva <aziza@Aziza-Zhalgaspayeva.local>
2024-02-07 13:04:04 +0600

    File serialization

--- main/serializers.py
+++ main/serializers.py
@@ -1,8 +1,30 @@
+from django.core.exceptions import ValidationError
+from django.core.files.base import File
+from django.core.validators import FileExtensionValidator
 from rest_framework import serializers
+
 from .models import User, Task, Tag
+from task_manager import settings
+
+
+class FileMaxSizeValidator:
+    def __init__(self, max_size: int) -> None:
+        self.max_size = max_size
+
+    def __call__(self, value: File) -> None:
+        if value.size > self.max_size:
+            raise ValidationError(f"Maximum size {self.max_size} exceeded.")
 
 
 class UserSerializer(serializers.ModelSerializer):
+    avatar_picture = serializers.FileField(
+        required=False,
+        validators=[
+            FileMaxSizeValidator(settings.UPLOAD_MAX_SIZES["avatar_picture"]),
+            FileExtensionValidator(["jpeg", "jpg", "png"]),
+        ]
+    )
+
     class Meta:
         model = User
         fields = (
@@ -11,6 +33,7 @@ class UserSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "email",
+            "avatar_picture",
         )
 
 
