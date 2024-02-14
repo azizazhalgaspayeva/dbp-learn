8420e562996a603600309d4809788be7fb152784
Aziza Zhalgaspayeva <aziza@Aziza-Zhalgaspayeva.local>
2024-02-14 12:02:52 +0600

    Fix tests

--- test/test_user.py
+++ test/test_user.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.forms.models import model_to_dict
 from rest_framework.exceptions import ErrorDetail
 
 from test.base import TestViewSetBase
@@ -10,6 +11,7 @@ from test.base.factories import UserFactory
 class TestUserViewSet(TestViewSetBase):
     basename = "users"
     auth_error = {"detail": ErrorDetail(string="Authentication credentials were not provided.", code="not_authenticated")}
+    required_fields = ["username", "first_name", "last_name", "email"]
 
     @staticmethod
     def expected_details(entity: dict, attributes: dict) -> dict:
@@ -20,21 +22,24 @@ class TestUserViewSet(TestViewSetBase):
         }
 
     def test_create_success(self) -> None:
-        user_attributes = UserFactory.build()
+        user_faker = UserFactory.build()
+        user_attributes = model_to_dict(user_faker, fields=self.required_fields)
         user = self.create(data=user_attributes, is_auth=True)
         expected_response = self.expected_details(user, user_attributes)
 
         assert user == expected_response
     
     def test_create_fail(self) -> None:
-        user_attributes = UserFactory.build()
+        user_faker = UserFactory.build()
+        user_attributes = model_to_dict(user_faker, fields=self.required_fields)
         response = self.create(data=user_attributes)
         expected_response = self.auth_error
 
         assert response == expected_response
 
     def test_list_success(self) -> None:
-        user_attributes = UserFactory.build()
+        user_faker = UserFactory.build()
+        user_attributes = model_to_dict(user_faker, fields=self.required_fields)
         self.create(data=user_attributes, is_auth=True)
         response = self.list(is_auth=True)[-1]
         expected_response = self.expected_details(response, user_attributes)
@@ -42,7 +47,8 @@ class TestUserViewSet(TestViewSetBase):
         assert response == expected_response
 
     def test_list_fail(self) -> None:
-        user_attributes = UserFactory.build()
+        user_faker = UserFactory.build()
+        user_attributes = model_to_dict(user_faker, fields=self.required_fields)
         self.create(data=user_attributes, is_auth=True)
         response = self.list()
         expected_response = self.auth_error
@@ -50,7 +56,8 @@ class TestUserViewSet(TestViewSetBase):
         assert response == expected_response
 
     def test_retrieve_success(self) -> None:
-        user_attributes = UserFactory.build()
+        user_faker = UserFactory.build()
+        user_attributes = model_to_dict(user_faker, fields=self.required_fields)
         user = self.create(data=user_attributes, is_auth=True)
         response = self.retrieve(is_auth=True, args=user["id"])
         expected_response = self.expected_details(response, user_attributes)
@@ -58,7 +65,8 @@ class TestUserViewSet(TestViewSetBase):
         assert response == expected_response
     
     def test_retrieve_fail(self) -> None:
-        user_attributes = UserFactory.build()
+        user_faker = UserFactory.build()
+        user_attributes = model_to_dict(user_faker, fields=self.required_fields)
         user = self.create(data=user_attributes, is_auth=True)
         response = self.retrieve(args=user["id"])
         expected_response = self.auth_error
@@ -66,7 +74,8 @@ class TestUserViewSet(TestViewSetBase):
         assert response == expected_response
 
     def test_delete_success(self) -> None:
-        user_attributes = UserFactory.build()
+        user_faker = UserFactory.build()
+        user_attributes = model_to_dict(user_faker, fields=self.required_fields)
         user = self.create(user_attributes, is_auth=True)
         response = self.delete(is_admin=True, args=user["id"])
         expected_response = None
@@ -74,7 +83,8 @@ class TestUserViewSet(TestViewSetBase):
         assert response == expected_response
 
     def test_delete_fail(self) -> None:
-        user_attributes = UserFactory.build()
+        user_faker = UserFactory.build()
+        user_attributes = model_to_dict(user_faker, fields=self.required_fields)
         user = self.create(user_attributes, is_auth=True)
         response = self.delete(args=user["id"])
         expected_response = self.auth_error
@@ -82,25 +92,32 @@ class TestUserViewSet(TestViewSetBase):
         assert response == expected_response
     
     def test_filter_success(self) -> None:
-        user_attributes = UserFactory.build()
+        user_faker = UserFactory.build()
+        user_attributes = model_to_dict(user_faker, fields=self.required_fields)
+        first_name = user_attributes["first_name"]
+
         self.create(user_attributes, is_auth=True)
-        response = self.filter(args="?first_name=oh")[-1]
+        response = self.filter(args="?first_name=" + first_name)[-1]
         expected_response = self.expected_details(response, user_attributes)
 
         assert response == expected_response
 
     def test_large_avatar(self) -> None:
-        user_attributes = UserFactory.build(
+        user_faker = UserFactory.build(
             avatar_picture=SimpleUploadedFile("large.jpg", b"x" * 2 * 1024 * 1024)
         )
-        response = self.request_create(user_attributes)
+        user_attributes = model_to_dict(user_faker, fields=self.required_fields + ["avatar_picture"])
+        response = self.action_client.request_create_user(**user_attributes)
         assert response.status_code == HTTPStatus.BAD_REQUEST
         assert response.json() == {"avatar_picture": ["Maximum size 1048576 exceeded."]}
 
     def test_avatar_bad_extension(self) -> None:
-        user_attributes = UserFactory.build()
+        user_faker = UserFactory.build()
+        user_attributes = model_to_dict(user_faker, fields=self.required_fields + ["avatar_picture"])
         user_attributes["avatar_picture"].name = "bad_extension.pdf"
-        response = self.request_create(user_attributes)
+
+        response = self.action_client.request_create_user(**user_attributes)
+
         assert response.status_code == HTTPStatus.BAD_REQUEST
         assert response.json() == {
             "avatar_picture": [
